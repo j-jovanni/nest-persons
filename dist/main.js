45dd6aa6347f31fc20641195d3bd010777786388
JovanniF <kapitaldev2@kapital.mx>
2023-03-16 19:04:01 -0600

    masss

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@ async function bootstrap() {
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
     }));
-    await app.listen(8081);
+    await app.listen(8080);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
